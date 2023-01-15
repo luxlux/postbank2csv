0d4490818e08f481a8b76d8ffc3b2f9a5f94f236
luxlux <luxlux@users.noreply.github.com>
2023-01-15 15:01:03 +0100

    Update postbank2csv.py
    Unterteilung von Detail Informationen in zusätzlich Spalten

--- postbank2csv.py
+++ postbank2csv.py
@@ -5,6 +5,7 @@ import csv
 import subprocess
 import sys
 import tempfile
+import re
 
 parser = argparse.ArgumentParser(description='Convert Postbank account statement pdf files to a single csv file.')
 parser.add_argument('pdf_files', metavar='pdf_file', type=argparse.FileType('r'), nargs='+', help='pdf files to convert')
@@ -14,9 +15,44 @@ args = parser.parse_args()
 def main():
     statements = []
 
+    # class to collect data for sorting file names by date in name
+    class myFile:
+        def __init__(self, name, sortable):
+            self.name = name
+            self.sortable = sortable
+        def __repr__(self): # steuert ausgabe z.b. mittels print
+            return str(self.name + " " + self.sortable)  
+
+    files = []
+
+    # get about date in name of Auszug
     for file in args.pdf_files:
+        
         #print(file.name)
+        # works with filenames with a date in it like this:
+        x = re.search(r"(\d{2})-(\d{2})-(\d{4})", file.name) # "PB_Kontoauszug_KtoNr_012334455_05-12-2022_104523.pdf"
+        if x is None:
+            x = re.search(r"((\d{4})-\d{2})-(\d{2})", file.name) # "Konto_012334455_2023-01-01_PB34553393.pdf"
+            if x is None:
+                sortable = 0 # casse no date found in filename
+            else:    
+                sortable =  x.group(1) + x.group(2) + x.group(3)
+        else:
+            sortable = x.group(3) + x.group(2) + x.group(1)
+
+        #print(sortable)
+        files.append(myFile(file.name,sortable))
+    
+    def get_sortable(myfile):
+        return myfile.sortable
+
+    files.sort(key=get_sortable, reverse=False)
+    #print(files)
+
+    for file in files:
+        
         statements += parse_statements_from_file(str(file.name))
+        #print(statements)
 
     write_statements_as_csv(statements)
 
@@ -29,21 +65,111 @@ def get_between (text, first_needle, second_needle):
     else:
         first_needle_idx = None
         try:
-            first_needle_idx = text.index(first_needle)
+            first_needle_idx = text.lower().index(first_needle.lower())
+            #print("FIRST:",first_needle,first_needle_idx, "TXT:", text)
         except ValueError:
-            first_needle_idx = text.index(first_needle.replace("-", ""))
+            first_needle_idx = text.lower().index(first_needle.lower().replace("-", ""))
 
     if second_needle is None:
         second_needle_idx = -1
     else:
         second_needle_idx = None
         try:
-            second_needle_idx = text.index(second_needle)
+            second_needle_idx = text.lower().index(second_needle.lower(), first_needle_idx + len(first_needle) + 1) #suche ab nach dem 1. Zeichen (=Leerzeichen) nach fist needle
         except ValueError:
-            second_needle_idx = text.index(second_needle.replace("-", ""))
+            second_needle_idx = text.lower().index(second_needle.lower().replace("-", ""),first_needle_idx + len(first_needle) + 1)
+    
+    mytxt = text[first_needle_idx + len(first_needle):second_needle_idx].strip()
+    #print(first_needle_idx ,second_needle_idx ,">> ",mytxt )
 
-    return text[first_needle_idx + len(first_needle):second_needle_idx].strip()
+    return mytxt
 
+# Liste mit regelmäßig vorkommenden Namen, z.B. in Daueraufträgen, um diese Namen vom Begin des Verwendungszweckes zu entfernen und in "Empfänger" zu übernehmen
+names_list = ["MIETERKONTO", "Advanzia Bank S.A", "Amazon.de KartenService", "Rundfunk ARD, ZDF, DRadio"] # liste der empänger von Daueraufträge, für die Namenstrennung aus Verwendungszweck
+key_words0 = ["Referenz", "Mandat", "Einreicher-ID","Verwendungszweck"]
+keys = []
+# keys.append({"word":"Referenz", "extract":"next"})
+# keys.append({"word":"Mandat", "extract":"next"})
+# keys.append({"word":"Einreicher-ID", "extract":"next"})
+# keys.append({"word":"Verwendungszweck", "extract":"next"})
+
+class key:
+    def __init__(self, word, field_name, extract):
+        self.word = word
+        self.field_name = field_name
+        self.extract = extract
+    def __repr__(self): # steuert ausgabe z.b. mittels print
+        return str(self.word + " " + self.extract)  
+
+keys.append(key("Referenz","Empfaenger","beforeKey"))
+keys.append(key("Referenz","Referenz","untilNextKey"))
+keys.append(key("Mandat","Mandat","untilNextKey"))
+keys.append(key("Einreicher-ID","Einreicher-ID","naechsteWort"))
+keys.append(key("Einreicher-ID","Verwendungszweck","restOhneErstesWort")) # Rest der Buchung, ohne erstes Wort
+#keys.append(key("EinreicherID","Einreicher-ID","naechsteWort")) # manchmal fehlt der Bindestrich
+keys.append(key("Verwendungszweck","Verwendungszweck","untilNextKey"))
+
+# teilt die in der Buchung enthaltenen Infos in 'Zusammen' in weitere Spalten auf 
+def sub_parse_zusammen2(statement):
+    
+    # in buchung vorkommende keys ermitteln 
+    contained_keys = []
+    for key in keys:
+        if statement['Zusammen'].replace("-","").find(key.word.replace("-", "")) > -1: # search but irnore "-"
+            contained_keys.append(key)
+    #print("CON: ",len(contained_keys),contained_keys)
+
+    # wenn keine keys gefunden, zumindest namen aus names_list separieren
+    if len(contained_keys) < 1:
+        # alles in Verwendungszweck übernehmen
+        statement['Verwendungszweck'] = statement['Zusammen']
+        #print("VER:",statement['Verwendungszweck'])
+
+        for name in names_list:
+            if statement['Zusammen'].lower().find(name.lower()) == 0:
+                statement['Empfaenger'] = name
+                statement['Verwendungszweck'] = get_between(statement['Zusammen'],name, None)
+                #print("VERW:", statement['Verwendungszweck'])
+            
+    else:
+        # wenn nur 1 key gefunden, vor Schlüßel immer als Name und danach auch als Verwendungszweck verwenden
+        if len(contained_keys) == 1 or len(contained_keys) == 2 :
+            statement['Empfaenger'] = get_between(statement['Zusammen'],None ,contained_keys[0].word)
+            statement['Verwendungszweck'] = get_between(statement['Zusammen'], contained_keys[0].word, None)
+
+
+        #vorkommende keys zum separieren verwenden
+        for idx, key in enumerate(contained_keys):
+            if key.extract == "beforeKey":
+                statement[key.field_name] = get_between(statement['Zusammen'],None ,key.word)
+            if key.extract == "naechsteWort":
+                statement[key.field_name] = get_between(statement['Zusammen'],key.word ," ")    
+            if key.extract == "restOhneErstesWort": # Rest der Buchung, ohne erstes Wort
+                rest = get_between(statement['Zusammen'],key.word, None)
+                statement[key.field_name] = get_between(rest," ", None)    
+                            
+            if key.extract == "untilNextKey":
+                try:
+                    statement[key.field_name] = get_between(statement['Zusammen'],key.word ,contained_keys[idx+1].word)
+                    #print(statement[key.field_name])
+
+                except IndexError:
+                    #print(key.field_name, "IndexError: ",key.word, len(contained_keys), idx, contained_keys  )
+                    statement[key.field_name] = get_between(statement['Zusammen'],key.word, None)
+                    #print(statement[key.field_name])
+
+    if statement['Typ'] == "Zinsen/Entg.":
+        #print("ZIN:",statement)
+        statement['Verwendungszweck'] = statement['Typ']
+        statement['Empfaenger'] = "Postbank"
+    
+    if statement['Typ'] == "Rechnungsabschluss -" and statement['Betrag'] == "sieheHinweis":
+        statement['Betrag'] = "" 
+        
+
+    return statement      
+
+# OLD Version - not in use
 def sub_parse_zusammen(statement):
 
     if statement['Typ'] == "SDD Lastschr" or statement['Typ'] == "Kartenzahlung":
@@ -58,7 +184,13 @@ def sub_parse_zusammen(statement):
         statement['Empfaenger'] = get_between(statement['Zusammen'], None,"Referenz")
         statement['Verwendungszweck'] = get_between(statement['Zusammen'],"Verwendungszweck",None)
         statement['Referenz'] = get_between(statement['Zusammen'],"Referenz","Verwendungszweck")
-
+    
+    # ausgewählte Empfänger aus dauerauftrag_list erkennen und abtrennen
+    elif statement['Typ'].find("Dauerauftrag") == 0 or statement['Typ'] == "SEPA Überw. Einzel":
+        for name in names_list:
+            if statement['Zusammen'].find(name) == 0:
+                statement['Empfaenger'] = name
+                statement['Verwendungszweck'] = get_between(statement['Zusammen'], name, None)
     else:
         statement['Verwendungszweck'] = statement['Zusammen']
 
@@ -116,7 +248,7 @@ def parse_statements_from_file(pdf_filename):
             if not line_token:
                 in_statement = False
                 if statement: # if dict not empty
-                    statements.append(sub_parse_zusammen(statement))
+                    statements.append(sub_parse_zusammen2(statement))
                     #print("new statement written", statement)
                     statement = {}
 
@@ -167,10 +299,13 @@ def parse_statements_from_file(pdf_filename):
 
 def write_statements_as_csv(statements):
 
-    fieldnames = ['Buchungsdatum','Empfaenger' ,'Verwendungszweck', 'Betrag', 'Typ', 'Einreicher-id', 'Referenz', 'Mandat' ,'Zusammen']
+    fieldnames = ['Buchungsdatum','Empfaenger' ,'Verwendungszweck', 'Notiz', 'Betrag', 'Typ', 'Einreicher-ID', 'Referenz', 'Mandat' ,'Zusammen']
     writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames, restval='',extrasaction='ignore')
     writer.writeheader()
-    for statement in statements:
+
+    statementsReversed = reversed(statements) # Reihenfolge der Buchungen umdehen
+
+    for statement in statementsReversed:
         writer.writerow(statement)
 
 
